c08074cd0698ee9577bf3f374d68485346dab9cb
AccelAnalysis <jholman@accelanalysis.com>
2026-08-21 16:39:16 -0400

    Export all Facility nested preview routes

--- app/(workspace)/[workspace]/[[...slug]]/page.tsx
+++ app/(workspace)/[workspace]/[[...slug]]/page.tsx
@@ -1,13 +1,22 @@
 import { WorkspaceRoute } from "@/components/workspace-route";
+import { referenceFacilityContext } from "@/fixtures/reference-data";
+import { getFacilityStaticRouteSlugs } from "@/lib/facility-navigation";
 import { workspaceIds, workspaceNavigation } from "@/lib/navigation";
 
 export function generateStaticParams() {
-  return workspaceIds.flatMap((workspace) =>
-    workspaceNavigation[workspace].map((item) => ({
+  return workspaceIds.flatMap((workspace) => {
+    if (workspace === "facility") {
+      return getFacilityStaticRouteSlugs(
+        referenceFacilityContext.programRunId,
+        referenceFacilityContext.participantId
+      ).map((slug) => ({ workspace, slug }));
+    }
+
+    return workspaceNavigation[workspace].map((item) => ({
       workspace,
       slug: item.slug ? item.slug.split("/") : []
-    }))
-  );
+    }));
+  });
 }
 
 export default function WorkspacePage() {
